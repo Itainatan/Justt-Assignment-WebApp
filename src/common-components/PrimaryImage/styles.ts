cd163a764367d64689030ff54cd16384a1c9e5e1
itai natan <itai08425@gmail.com>
2023-10-16 20:07:30 +0300

    updates

--- src/common-components/PrimaryImage/styles.ts
+++ src/common-components/PrimaryImage/styles.ts
@@ -2,7 +2,8 @@ import { css } from "@emotion/react";
 
 export const container = css`
   width: 100%;
-  height: 8vh;
+  min-height: 8vh;
+  max-height: 20vh;
   position: relative;
   overflow: hidden;
 `;
